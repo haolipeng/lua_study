d5f209c15b53d13b719790204440e30f9960c019
codergeek <1078285863@qq.com>
2021-09-04 11:31:17 +0800

    算术运算符

---
+++ operator/math.lua
@@ -0,0 +1,23 @@
+a = 21
+b = 10
+
+c = a + b
+print("Line 1 - c 的值为 ", c ) --31
+
+c = a - b
+print("Line 2 - c 的值为 ", c ) --11
+
+c = a * b
+print("Line 3 - c 的值为 ", c ) --210
+
+c = a / b
+print("Line 4 - c 的值为 ", c ) --2.1
+
+c = a % b
+print("Line 5 - c 的值为 ", c ) --1
+
+c = a^2
+print("Line 6 - c 的值为 ", c ) --441
+
+c = -a
+print("Line 7 - c 的值为 ", c ) --420
