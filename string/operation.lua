e84e80ef2f7a2a3f51b7331cb0c1564d2a4c5c34
codergeek <1078285863@qq.com>
2021-09-06 15:17:32 +0800

    modify

--- string/operation.lua
+++ string/operation.lua
@@ -1,14 +1,18 @@
 name = "haolipeng"
 
+--转换为大写
 upperName = string.upper(name)
 print(upperName)
 
+--转换为小写
 lowerName = string.lower(upperName)
 print(lowerName)
 
+--反转
 reverseName = string.reverse(name)
 print(reverseName)
 
+--在字符串中替换，3为替换次数
 newStr = string.gsub("aaaa","a","z",3);
 print(newStr)
 
@@ -16,11 +20,33 @@ print(newStr)
 formatStr = string.format( "the value is %d",4)
 print(formatStr)
 
-findStr = string.find( "Hello Lua user","Lua",1)
-print(findStr)
+-- 在目标字符串中搜索指定的内容
+pos = string.find( "Hello Lua user","Lua",1)
+print("str find pos:",pos)
 
 --char 将整型数字转成字符并连接
 --byte 转换字符为整数值(可以指定某个字符，默认第一个字符)。
 print(string.char(97,98,99,100))
 print(string.byte("ABCD",4))    --可指定将某个字符转化为整数
 print(string.byte("ABCD"))      --不指定的话，默认第一个字符
+
+print("haolipeng len:",string.len("haolipeng"))
+
+print("repeat string:",string.rep( "abcd", 2))
+
+print("www.baidu"..".com")
+
+local sourcestr = "prefix--runoobgoogletaobao--suffix"
+print("\n原始字符串", string.format("%q", sourcestr))
+
+local first_sub = string.sub(sourcestr, 4, 15)
+print("\n第一次截取", string.format("%q", first_sub)) --lua的索引下标是从1开始的
+
+local second_sub = string.sub(sourcestr, 1, 8)
+print("\n第二次截取", string.format("%q", second_sub))
+
+local third_sub = string.sub(sourcestr, -10)
+print("\n第三次截取", string.format("%q", third_sub))
+
+local fourth_sub = string.sub(sourcestr, -100)
+print("\n第四次截取", string.format("%q", fourth_sub)) --索引越界，输出原始字符串
